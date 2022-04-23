54f83c54a1724dbb13a449ab1ed5a8a6dc2633c5
Janiita Khan <104274208+Janiita@users.noreply.github.com>
2022-04-23 12:26:04 -0700

    Create if_else_if.cpp

---
+++ if_else_if.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  if(true)
+  {
+    cout<<"This is 1st if"<<endl;
+  }
+  else if(true)
+  {
+    cout<<"This is 2nd if"<<endl;
+  }
+  else if(true)
+  {
+     cout<<"This is 3rd if"<<endl;
+  }
+  else
+  {
+    cout<<"This is else body"<<endl;
+  }
