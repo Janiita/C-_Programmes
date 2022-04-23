f55ebb713454a67658e9eb9716a7d4c1e28dac61
Janiita Khan <104274208+Janiita@users.noreply.github.com>
2022-04-23 12:21:23 -0700

    Create if&else.cpp

---
+++ if&else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	
+	cout<<"Enter value of a"<<endl;
+	cin>>a;
+	cout<<"Enter value of b"<<endl;
+	cin>>b;
+	
+	if(a>=b)
+	{
+		cout<<"a is greater than or eqyual to b"<<endl;
+	}
+	else
+	{
+		cout<<"a is less than b"<<endl;
+	}
+}
