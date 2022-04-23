94880c58dd74550c7b8b0674b9391bc0549e8dec
Janiita Khan <104274208+Janiita@users.noreply.github.com>
2022-04-23 12:10:00 -0700

    Create short-hand-if.cpp

---
+++ short-hand-if.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int valueOne= 5;
+	int valueTwo= 10;
+	int answer;
+	
+	answer = (valueOne<valueTwo)? 5:6;
+	cout<<answer;
+	
+	return 0;
+}
