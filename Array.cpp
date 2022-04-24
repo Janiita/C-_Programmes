e7a9a2eb1523a754b333ca67530af84e66cd40ef
Janiita Khan <104274208+Janiita@users.noreply.github.com>
2022-04-24 08:19:20 -0700

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[3];
+  a[0]=6;
+  a[1]=7;
+  a[2]=10;
+  a[1]=15;
+  cout<<a[1];
+  cin>>a[0];
+  cout<<a[0];
+  
+}
+//Code by Janiita Khan
