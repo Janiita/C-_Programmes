470cfb8cc193512aa1a9dd61a7741037fd5f47d7
Janiita Khan <104274208+Janiita@users.noreply.github.com>
2022-04-30 14:23:42 -0700

    Create if_LN.cpp

---
+++ if_LN.cpp
@@ -0,0 +1,29 @@
+//Program to find largest number among three numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,n3;
+    
+    cout<<"Enter three numbers:"<<endl;
+    cin>>n1>>n2>>n3;
+    
+    if(n1>=n2 && n1>n3)
+    {
+       cout<<"Largest number: "<<n1;
+    }
+    
+    if(n2>=n1 && n2>=n3)
+    {
+       cout<<"Largest number: "<<n2;
+    }
+    
+    if(n3>=n1 && n3>=n2)
+    {
+       cout<<"Largest number: "<<n3;
+    }
+    
+  return 0;
+	
+}
+//Code by Janiita Khan
