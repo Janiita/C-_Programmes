fcf96e8084f463530c0ce6b766cfedad304f4c93
Janiita Khan <104274208+Janiita@users.noreply.github.com>
2022-05-01 11:20:57 -0700

    Create increment p&s.cpp

---
+++ increment p&s.cpp
@@ -0,0 +1,15 @@
+//Program for prefix and suffix increment
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x= 3, y, z; //variable initialization & declaration
+	
+	y=x++;
+	z=++x;
+	
+	cout<<x<<","<<y<<","<<z;
+	
+	return 0;
+}
+//Code by Janiita Khan
