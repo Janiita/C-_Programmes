b36dde63ed29a4b086683f915649705eaa153cc5
Janiita Khan <104274208+Janiita@users.noreply.github.com>
2022-05-25 01:40:36 -0700

    Create Do-While.cpp

---
+++ Do-While.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a= 10;  // Local Variable declaration
+	//do loop execution
+	do{
+		cout<<"value of a:"<<a<<endl;
+		
+		a=a+1;
+		
+	}while(a<20);
+	
+	return 0;
+}
+//Code by Janiita Khan
